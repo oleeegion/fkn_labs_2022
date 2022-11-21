a1df18613d08f955a210d984b14d1e6e6967e220
oleeegion <timofeeva_2001@mail.ru>
2022-11-21 20:08:14 +0600

    add screen hero

--- lib/constans.dart
+++ lib/constans.dart
@@ -8,9 +8,28 @@ const TextStyle standartStyle =
 const TextStyle titleStyle =
     TextStyle(color: Colors.white, fontSize: 25, fontWeight: FontWeight.bold);
 
+const TextStyle subtitleStyle =
+    TextStyle(color: Colors.white, fontSize: 22, fontWeight: FontWeight.bold);
+
 final List<Heroes> marvelHeroes = [
-  Heroes('Capitan America', '', 'assets/cap_america.jpg', Colors.brown),
-  Heroes('Spider Man', '', 'assets/spider_man.jpg', Colors.blueGrey),
-  Heroes('Vision', '', 'assets/vision.jpg', Colors.deepOrange),
-  Heroes('Ant-Man', '', 'assets/ant_man.jpg', Colors.teal),
+  Heroes(
+      'Capitan America',
+      'I am Capitan America!',
+      'https://i.pinimg.com/originals/d7/bb/93/d7bb93ead29f8296df1d7290d49449f6.jpg',
+      Colors.brown),
+  Heroes(
+      'Spider Man',
+      'У всех есть тайны. Что-то скрываем мы, что-то скрывают от нас.',
+      'https://phonoteka.org/uploads/posts/2021-07/thumbs/1625699340_6-phonoteka-org-p-chelovek-pauk-krutoi-art-krasivo-6.jpg',
+      Colors.blueGrey),
+  Heroes(
+      'Vision',
+      'Я стрелял в того, кого искренне считал своим другом.',
+      'https://i.pinimg.com/originals/d6/08/7e/d6087e3bfaa62a7c94be7fdc8bc7fab1.jpg',
+      Colors.deepOrange),
+  Heroes(
+      'Ant-Man',
+      'Я свято верю в право каждого на искупление.',
+      'https://i.pinimg.com/originals/c4/77/88/c4778879adfca03e7f82e5084bfc2731.jpg',
+      Colors.teal),
 ];
--- lib/widgets/card_hero.dart
+++ lib/widgets/card_hero.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_2/constans.dart';
+import 'package:flutter_application_2/widgets/screen_hero.dart';
 
 import '../models/heroes.dart';
 
@@ -12,26 +13,38 @@ class CardHero extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
         padding: const EdgeInsets.symmetric(horizontal: 10),
-        child: Card(
-            elevation: 0.6,
-            clipBehavior: Clip.hardEdge,
-            shape:
-                RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
-            child: Stack(fit: StackFit.expand, children: [
-              SizedBox.expand(
-                  child: Image.asset(
-                _hero.pathImage,
-                fit: BoxFit.cover,
-              )),
-              Positioned(
-                  bottom: 10,
-                  child: Padding(
-                    padding: const EdgeInsets.all(10),
-                    child: Text(
-                      _hero.name,
-                      style: titleStyle,
-                    ),
+        child: GestureDetector(
+          onTap: () => _clickHero(context),
+          child: Hero(
+            tag: _hero.name,
+            child: Card(
+                elevation: 0.6,
+                clipBehavior: Clip.hardEdge,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20)),
+                child: Stack(fit: StackFit.expand, children: [
+                  SizedBox.expand(
+                      child: Image.network(
+                    _hero.pathImage,
+                    fit: BoxFit.cover,
                   )),
-            ])));
+                  Positioned(
+                      bottom: 10,
+                      child: Padding(
+                        padding: const EdgeInsets.all(10),
+                        child: Text(
+                          _hero.name,
+                          style: titleStyle,
+                        ),
+                      )),
+                ])),
+          ),
+        ));
+  }
+
+  void _clickHero(context) {
+    Navigator.of(context).push(MaterialPageRoute(
+      builder: (BuildContext context) => ScreenHero(_hero),
+    ));
   }
 }
